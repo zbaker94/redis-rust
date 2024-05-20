6880fd7dc19dcba25145ac4765aabccf0cbef86c
zbaker94 <zbaker94@gmail.com>
2024-05-20 01:13:57 -0400

    fix compilation errors

--- src/serialization/serialize.rs
+++ src/serialization/serialize.rs
@@ -1,9 +1,9 @@
 use crate::redis_data_type::RedisDataType;
 
 fn redis_string_factory(data: RedisDataType){
-    
+
 }
 
 pub fn main(data: RedisDataType) -> String {
-
+    return String::new();
 }
